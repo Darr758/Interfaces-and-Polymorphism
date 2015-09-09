d675eff9d966d5dc637545a7a3676757b45358c5
Darr758 <Darraghk500@yahoo.ie>
2015-09-09 23:22:42 +0100

    Create Consumable.java

---
+++ Consumable.java
@@ -0,0 +1,10 @@
+/**
+*	An interface that declares the methods getName() and decideTaste();
+*
+*	@author Darragh King (ID 113372871)
+*/
+
+public interface Consumable {
+	public String getName();
+	public String decideTaste();
+} 
