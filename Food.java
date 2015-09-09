5bfd83e491f0b9a7961e12c84bf2a55126aa3ab2
Darr758 <Darraghk500@yahoo.ie>
2015-09-09 23:20:29 +0100

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,7 @@
+/**
+*	This interface groups all of the foods together
+*	so that they can be identified as a food in the menu itself.
+*
+*	@author Darragh King (ID 113372871)
+*/
+public interface Food extends MenuItems{ } 
