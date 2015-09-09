6462d3361d25ff378bb371ab8cd6782b9b4bcc19
Darr758 <Darraghk500@yahoo.ie>
2015-09-09 23:18:22 +0100

    Create Beverage.java

---
+++ Beverage.java
@@ -0,0 +1,7 @@
+/**
+*	This interface groups all of the beverages together
+*	so that they can be identified as a beverage in the menu itself.
+*
+*	@author Darragh King (ID 113372871)
+*/
+public interface Beverage extends MenuItems{ }
